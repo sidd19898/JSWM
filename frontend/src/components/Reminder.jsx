899abffb1e00f01158feabfdcdf5e506a222fed7
sidd19898 <anganesiddhu@gmail.com>
2026-06-07 11:17:10 +0530

    added reminder component

--- frontend/src/components/Reminder.jsx
+++ frontend/src/components/Reminder.jsx
@@ -2,14 +2,13 @@ import "../components/allinone.css"
 
 export function Reminder({children1,children2}) {
   return (
-    <div className="dosj">
+    
 <div className="remindar">
       {children1}
-      </div>
       <div className="numbers">
       {children2}
       </div>
-    </div>
+      </div>
     
   )
 }
