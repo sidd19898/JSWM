9c67216bb6b83a33ba686e6bdcaa345af1ef2496
Siddhant Angane <anganesiddhu@gmail.com>
2026-06-13 07:47:33 +0530

    now date can be edited using arrow and also it can be added using calendar

--- frontend/src/components/Addtask.jsx
+++ frontend/src/components/Addtask.jsx
@@ -1,11 +1,17 @@
 import "../components/allinone.css"
 import ClearIcon from '@mui/icons-material/Clear';
 import { Button3 } from "./Button3";
+import { useState } from "react";
+import TimePicker from 'react-time-picker';
 
 export function Addtask({isActive,setisActive}){
     function godsep(){
     setisActive(true);
     }
+
+const [currentDate, setCurrentDate] = useState(new Date());
+const [value, onChange] = useState('10:00');
+
     return(
         <div className={`addtask ${!isActive ? "is-visible" : ""}`}>
             <div className="end">
@@ -31,12 +37,13 @@ export function Addtask({isActive,setisActive}){
             <div className="bshfs">
                 <div className="kjdf">To</div>
                 <div>:</div>
-                <div><input className="fgk"></input></div>
+                <div><TimePicker onChange={onChange} value={value} /></div>
             </div>
             <div className="bshfs">
                 <div className="kjdf">Date</div>
                 <div>:</div>
-                <div><input className="fgk"></input></div>
+                <div><input className="fgk" type="date" value={currentDate.toISOString().split('T')[0]}
+                onChange={(e) => setCurrentDate(new Date(e.target.value))}></input></div>
             </div>
 
             </div>
--- frontend/src/components/Taskview.jsx
+++ frontend/src/components/Taskview.jsx
@@ -3,15 +3,36 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { Secondside } from "./Secondside";
+import { useState } from "react";
 
 export function Taskview() {
+
+  const [currentDate, setcurrentDate] = useState(new Date());
+
+  const changeDate = (daysAmount) => {
+    setcurrentDate((prevDate) => {
+      const newDate = new Date(prevDate);
+      newDate.setDate(newDate.getDate()+ daysAmount);
+      return newDate;
+    })
+  }
   return (
     <div className="taskview">
       <div className="topside">
           <div className="bin"><DeleteIcon sx={{fontSize:"5vmin"}}></DeleteIcon></div>
-          <div>hi there</div>
-          <div><ArrowBackIcon sx={{fontSize:"5vmin"}}></ArrowBackIcon>
-               <ArrowForwardIcon sx={{fontSize:"5vmin"}}></ArrowForwardIcon></div>
+          <div className="today">
+            <input
+  type="date"
+  value={currentDate.toISOString().split("T")[0]}
+  min={currentDate.toISOString().split("T")[0]}
+  max="2026-12-31"
+  className="cal"
+/>
+          </div>
+          <div>
+            <button onClick={() => changeDate(-1)}><ArrowBackIcon sx={{fontSize:"5vmin"}}></ArrowBackIcon></button>
+            <button onClick={() => changeDate(1)}><ArrowForwardIcon sx={{fontSize:"5vmin"}}></ArrowForwardIcon></button>
+          </div>
       </div>
       <div>
         <Secondside children1={"hi"} children2={"hi"} children3={"hi"}></Secondside>
